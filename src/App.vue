5295c4fdb8c7c88479ca11258e28cdb5d56d1c75
direboar <minokuba@example.com>
2018-03-21 01:25:40 +0900

    画面レイアウトを修正（ピンク表示が気に入らないので修正、メニューバーの色をなんとなく修正）

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <v-app>
-    <v-navigation-drawer persistent :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" enable-resize-watcher fixed app>
-      <v-list>
-        <v-list-tile value="true" v-for="(item, i) in items" :key="i" @click="route(item)">
+    <v-navigation-drawer persistent :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" enable-resize-watcher fixed app width="220">
+      <v-list dense>
+        <v-list-tile v-for="(item, i) in items" :key="i" @click="route(item)">
           <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
+            <v-icon>{{item.icon}}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            <v-list-tile-title>{{item.title}}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar dense app :clipped-left="clipped">
+    <v-toolbar color="indigo" dark dense app :clipped-left="clipped">
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
@@ -34,21 +34,21 @@ export default {
   data() {
     return {
       clipped: false,
-      drawer: true,
+      drawer: false,
       fixed: false,
       items: [
         {
-          icon: "bubble_chart",
+          icon: "note",
           title: "イニシアチブボード",
           linkTo: "InitiativeBoard"
         },
         {
-          icon: "bubble_chart",
+          icon: "mode_edit",
           title: "能力値編集",
           linkTo: "AbilityEditor"
         },
         {
-          icon: "bubble_chart",
+          icon: "find_in_page",
           title: "呪文検索",
           linkTo: "SpellSerachTable"
         }
