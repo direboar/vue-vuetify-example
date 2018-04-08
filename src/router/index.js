96d5b87d326eb69e3dcbdd1d64be44b953786f42
direboar <minokuba@example.com>
2018-04-08 23:57:28 +0900

    IE11に対応（Polyfillを組み込み）

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ import IndexPage from '@/components/IndexPage'
 import Hoge from '@/components/Hoge'
 import Scroll from '@/components/Scroll'
 
+//ie11対応
+//see https://www.d-wood.com/blog/2016/04/12_7917.html
+//see https://qiita.com/terrierscript/items/d2a9d5d4daedaacff924
+import 'babel-polyfill'
+
 Vue.use(Router)
 
 export default new Router({
